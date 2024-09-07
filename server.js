92f066869eb44165f982078b30b8f40cf80c1ee9
thelogicguy <107140254+thelogicguy@users.noreply.github.com>
2024-09-07 21:56:58 +0100

    updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome Macdonald's simple Express server!');
+  res.send('Welcome to Macdonald's simple Express server!');
 });
 
 app.listen(port, () => {
