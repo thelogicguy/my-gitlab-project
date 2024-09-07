215015f97cbef903f25eb641f22ca9ff46e439f0
thelogicguy <107140254+thelogicguy@users.noreply.github.com>
2024-09-08 00:57:28 +0100

    modified server.js file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send(''<h1>Welcome to my simple Express server!</h1>'');
+  res.send('<h1>Welcome to my simple Express server!</h1>');
 });
 
 app.listen(port, () => {
